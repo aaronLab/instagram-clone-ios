713d05aadb8d94069a60ad72eb7392bfebef57ab
Aaron Lee <aaronlab.net@gmail.com>
2021-03-19 16:34:53 +0900

    config proj

---
+++ InstagramClone/Controller/MainTabController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabController.swift
+//  InstagramClone
+//
+//  Created by Aaron Lee on 2021-03-19.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+}
