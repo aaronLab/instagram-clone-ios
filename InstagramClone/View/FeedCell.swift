409fc62134cdd97157c5f1a4dec2f695e338f731
Aaron Lee <aaronlab.net@gmail.com>
2021-03-19 17:28:55 +0900

    feed cell ui

---
+++ InstagramClone/View/FeedCell.swift
@@ -0,0 +1,67 @@
+//
+//  FeedCell.swift
+//  InstagramClone
+//
+//  Created by Aaron Lee on 2021-03-19.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.isUserInteractionEnabled = true
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        return iv
+    }()
+    
+    private lazy var usernameButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("Venom", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 13)
+        button.addTarget(self, action: #selector(didTabUsername), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(
+            top: topAnchor,
+            left: leftAnchor,
+            paddingTop: 12,
+            paddingLeft: 12
+        )
+        profileImageView.setDimensions(height: 40, width: 40)
+        profileImageView.layer.cornerRadius = 40 / 2
+        
+        addSubview(usernameButton)
+        usernameButton.centerY(
+            inView: profileImageView,
+            leftAnchor: profileImageView.rightAnchor,
+            paddingLeft: 8
+        )
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Actions
+    
+    @objc func didTabUsername() {
+        print("DEBUG: did tap username")
+    }
+    
+}
