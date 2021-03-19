83aca088def9e245a5d889acb44c4ad360e998c5
Aaron Lee <aaronlab.net@gmail.com>
2021-03-19 21:21:16 +0900

    login page

---
+++ InstagramClone/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,22 @@
+//
+//  RegistrationController.swift
+//  InstagramClone
+//
+//  Created by Aaron Lee on 2021/03/19.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+    
+}
